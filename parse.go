9b892d7a469588f6c8bfdc7c2b44498b7464e09d
Austin Meier <austin.meier@hotmail.com>
2022-01-26 11:11:31 -0700

    Added Auction struct and loop to create objects

--- parse.go
+++ parse.go
@@ -9,6 +9,29 @@ import (
 
 	"github.com/valyala/fastjson"
 )
+type Bid struct {
+	bidder string
+	profile_id string
+	amount int
+	timestamp int
+}
+
+type Auction struct {
+	uuid string 
+	auctioneer string
+	profile_id string
+	start int
+	end int
+	item_name string
+	item_lore string
+	extra string
+	category string
+	tier string
+	starting_bid int
+	claimed bool
+	highest_bid_amount int
+	bids []Bid
+}
 
 func main() {
 
@@ -41,12 +64,35 @@ func main() {
 
 	//fmt.Printf("%s\n", body)
 
-	// v, err := fastjson.ParseBytes(body)
-	// if err != nil {
-	// 	log.Fatalf("cannot parse json: %s", err)
-	// }
+	v, err := fastjson.ParseBytes(body)
+	if err != nil {
+		log.Fatalf("cannot parse json: %s", err)
+	}
 
-	v:= fastjson.GetString(body, "auctions", "1", "item_name")
+	a := v.GetArray("auctions")
+	
+	var auctions []Auction
 
-	fmt.Printf("%s\n", v)
+	for i := 0; i < len(a); i++ {
+		var auc Auction = Auction {
+			uuid: string(a[i].GetStringBytes("uuid")),
+			auctioneer: string(a[i].GetStringBytes("auctioneer")),
+			profile_id: string(a[i].GetStringBytes("profile_id")),
+			start: a[i].GetInt("start"),
+			end: a[i].GetInt("end"),
+			item_name: string(a[i].GetStringBytes("item_name")),
+			item_lore: string(a[i].GetStringBytes("item_lore")),
+			extra: string(a[i].GetStringBytes("extra")),
+			category: string(a[i].GetStringBytes("category")),
+			tier: string(a[i].GetStringBytes("tier")),
+			starting_bid: a[i].GetInt("starting_bid"),
+			claimed: a[i].GetBool("claimed"),
+			highest_bid_amount: a[i].GetInt("highest_bid_amount"),
+			bids: nil,
+		}
+
+		auctions = append(auctions, auc)
+	}
+
+	fmt.Print("Auction Item: ", auctions[0])
 }
