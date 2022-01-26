94140d8b7165538ef240075218ccd63bbe20094b
Austin Meier <austin.meier@hotmail.com>
2022-01-26 10:23:31 -0700

    test pulling and parsing JSON from hypixel API

--- parse.go
+++ parse.go
@@ -11,12 +11,42 @@ import (
 )
 
 func main() {
-	
-	text, err := fastjson.Parser.Parse(`{"people": [{"craft": "ISS", "name": "Sergey Rizhikov"}, {"craft": "ISS", "name": "Andrey Borisenko"}, {"craft": "ISS", "name": "Shane Kimbrough"}, {"craft": "ISS", "name": "Oleg Novitskiy"}, {"craft": "ISS", "name": "Thomas Pesquet"}, {"craft": "ISS", "name": "Peggy Whitson"}], "message": "success", "number": 6}`)
-	if err != nil {
-		log.Fatalf("cannot parse json: %s", err)
+
+	url := "https://api.hypixel.net/skyblock/auctions"
+
+	client := http.Client{
+		Timeout: time.Second * 2, // Timeout after 2 seconds
 	}
 
-	fmt.Printf("%s\n", text)
-	
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "hypixel-auction-go")
+
+	res, getErr := client.Do(req)
+	if getErr != nil {
+		log.Fatal(err)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	//fmt.Printf("%s\n", body)
+
+	// v, err := fastjson.ParseBytes(body)
+	// if err != nil {
+	// 	log.Fatalf("cannot parse json: %s", err)
+	// }
+
+	v:= fastjson.GetString(body, "auctions", "1", "item_name")
+
+	fmt.Printf("%s\n", v)
 }
